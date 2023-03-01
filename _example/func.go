2984ddf3f9216f6135025e42cea624a8f0bb5ae9
Samuel Stoltenberg <sam@skelouse.com>
2023-02-28 23:58:51 -0600

    Fix scrolling below top

--- _example/func.go
+++ _example/func.go
@@ -1,9 +1,97 @@
 package example
 
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
 func Function(a bool) bool {
 	if a {
 		return false
 	}
 
 	return true
+}
+
+// https://github.com/XANi/loremipsum
+// LoremIpsum is a lorem ipsum generator
+type LoremIpsum struct {
+	first bool
+	words []string
+	idx   int
+	rng   *rand.Rand
+}
+
+// New returns new instance of LoremIpsum
+func New() *LoremIpsum {
+	return NewWithSeed(time.Now().Unix())
+}
+
+// New returns new instance of LoremIpsum with PRNG seeded with the parameter
+func NewWithSeed(seed int64) *LoremIpsum {
+	li := new(LoremIpsum)
+	li.rng = rand.New(rand.NewSource(seed))
+	li.first = true
+	li.idx = 0
+	li.shuffle()
+	return li
+}
+
+// Word returns a single word of lorem ipsum
+func (li *LoremIpsum) Word() string {
+	return li.words[li.rng.Intn(len(li.words))]
+}
+
+// WordList returns list of words of lorem ipsum
+func (li *LoremIpsum) WordList(count int) []string {
+	return li.words[:count]
+}
+
+// Words returns words of lorem ipsum
+func (li *LoremIpsum) Words(count int) string {
+	return strings.Join(li.WordList(count), " ")
+}
+
+// Sentence returns full sentence of lorem ipsum
+func (li *LoremIpsum) Sentence() string {
+	l := int(li.gauss(24.46, 5.08))
+	words := li.words[:l]
+	return li.punctuate(words)
+}
+
+// SentenceList returns list of sentences of lorem ipsum
+func (li *LoremIpsum) SentenceList(count int) []string {
+	var sentences []string
+	sentences = make([]string, count)
+	for idx := range sentences {
+		sentences[idx] = li.Sentence()
+		li.shuffle()
+	}
+	return sentences
+}
+
+// Sentences returns sentences of lorem ipsum
+func (li *LoremIpsum) Sentences(count int) string {
+	return strings.Join(li.SentenceList(count), " ")
+}
+
+// Paragraph returns full paragraph of lorem ipsum
+func (li *LoremIpsum) Paragraph() string {
+	return li.Sentences(int(li.gauss(5.8, 1.93)))
+}
+
+// ParagraphList returns list of paragraphs of lorem ipsum
+func (li *LoremIpsum) ParagraphList(count int) []string {
+	var paragraphs []string
+	paragraphs = make([]string, count)
+	for idx := range paragraphs {
+		paragraphs[idx] = li.Paragraph()
+	}
+	return paragraphs
+}
+
+// Paragraphs returns paragraphs of lorem ipsum
+func (li *LoremIpsum) Paragraphs(count int) string {
+	return strings.Join(li.ParagraphList(count), "\\n")
 }
